2f1cdc3beaa6396225bc6208547bb93973356b3e
ZSoJes <gomezsotelojuan@gmail.com>
2017-01-03 18:11:50 -0600

    ejemplo de pdf

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(title: 'frijoles', pricing: 15)
+Product.create(title: 'Cereal', pricing: 25)
+Product.create(title: 'tortillas', pricing: 12)
+Product.create(title: 'refresco', pricing: 16)
+Product.create(title: 'arroz', pricing: 10)
+Product.create(title: 'leche', pricing: 20)
